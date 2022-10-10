afb50e180d665a558837211f528e209aef6580e2
Dmitriy Voskoboynikov <voskoboynikov010684@gmail.com>
2022-10-10 23:36:13 +0300

    basename/fixed

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/goit-react-hw-08-phonebook/">
+        <BrowserRouter basename="goit-react-hw-08-phonebook">
           <App />
         </BrowserRouter>
       </PersistGate>
